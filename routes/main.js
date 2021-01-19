3affb7c590c0c5bd3a457ac0d2639ffcdc2953f5
nAzdAc <nAzdAc15071996@mail.ru>
2021-01-19 12:07:54 +0300

    feat: change folder server

---
+++ routes/main.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-underscore-dangle */
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const tokenList = {};
+const router = express.Router();
+
+router.get('/status', (req, res, next) => {
+  res.status(200);
+  res.json({ status: 'ok' });
+});
+
+router.post('/signup', passport.authenticate('signup', { session: false }), async (req, res, next) => {
+  res.status(200).json({ message: 'signup successful' });
+});
+
+router.post('/login', async (req, res, next) => {
+  // eslint-disable-next-line consistent-return
+  const { email, password } = req.body;
+  console.log(email);
+  if (email === 'test4@test.com' && password === '1234') {
+    const token = 'zalupa';
+    const refreshToken = '2zalupi';
+    res.status(200).json({
+      status: 'ok',
+      code: 200,
+      payload: {
+        token,
+        refreshToken,
+      },
+    });
+  } else {
+    res.status(401).json({ status: 'fail', code: 401, message: 'You are not authorized.' });
+  }
+
+  /*
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('An Error occured');
+        return next(error);
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const body = {
+          _id: user._id,
+          email: user.email,
+        };
+        console.log(body);
+        const token = jwt.sign({ user: body }, 'top_secret', { expiresIn: 300 });
+        const refreshToken = jwt.sign({ user: body }, 'top_secret_refresh', { expiresIn: 86400 });
+        // store tokens in cookie
+        res.cookie('jwt', token);
+        res.cookie('refreshJwt', refreshToken);
+        // store tokens in memory
+        tokenList[refreshToken] = {
+          token,
+          refreshToken,
+          email: user.email,
+          _id: user._id,
+        };
+        // Send back the token to the user
+        return res.status(200).json({ token, refreshToken });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+  */
+});
+
+router.post('/logout', (req, res) => {
+  if (req.cookies) {
+    const refreshToken = req.cookies.refreshJwt;
+    if (refreshToken in tokenList) delete tokenList[refreshToken];
+    res.clearCookie('refreshJwt');
+    res.clearCookie('jwt');
+  }
+  res.status(200).json({ message: 'logged out' });
+});
+
+router.post('/token', (req, res) => {
+  const { email, refreshToken } = req.body;
+  if ((refreshToken in tokenList) && (tokenList[refreshToken].email === email)) {
+    const body = { email, _id: tokenList[refreshToken]._id };
+    const token = jwt.sign({ user: body }, 'top_secret', { expiresIn: 300 });
+    // update jwt
+    res.cookie('jwt', token);
+    tokenList[refreshToken].token = token;
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const DOCROOT = './../dist/';
 
-// подключаем нужные модули
+// reads in our .env file and makes those values available as environment variables
+require('dotenv').config();
 
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const express = require('express');
+const bodyParser = require('body-parser');
 const http = require('http');
 const path = require('path');
-const express = require('express');
+const secureRoutes = require('./routes/secure');
+const routes = require('./routes/main');
 const sockets = require('./sockets');
 
 // создаём сервер, используя express и http
@@ -14,6 +21,12 @@ const sockets = require('./sockets');
 const app = express();
 const server = http.createServer(app);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 // настраиваем отдачу игровых файлов при запросе к серверу
 
@@ -21,15 +34,54 @@ const server = http.createServer(app);
 // const staticContent = express.static(documentRoot);
 // app.use(staticContent);
 
-
 // инициализируем сокеты
 sockets.init(server);
 
-app.get('/', (req, res, next) => {
-   res.send('hello world')
+// setup mongo connection
+const uri = process.env.MONGO_CONNECTION_URL;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+  process.exit(1);
+});
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo');
 });
 
-// запускаем серверт
-server.listen(PORT, () => {
-   console.log(`server is run http://localhost:${PORT}`)
+// update express settings
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+// require passport auth
+// require('./auth/auth');
+
+// app.get('/index.html', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   res.sendFile(`${DOCROOT}index.html`);
+// });
+
+app.use(express.static(DOCROOT));
+app.get('/', (req, res) => {
+  res.send('zalupa');
+});
+
+// routes
+app.use('/', routes);
+// app.use('/', passport.authenticate('jwt', { session: false }), secureRoutes);
+
+// catch all other routes
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ message: '404 - Not Found' });
+});
+
+// handle errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+// have the server start listening on the provided port
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
