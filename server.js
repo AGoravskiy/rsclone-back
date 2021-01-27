79a408b66074eb509324725d7655c4f8c1855777
Aleksey Goravskiy <32121103+AGoravskiy@users.noreply.github.com>
2021-01-27 23:40:00 +0300

    fix(cors): fix cors

--- server.js
+++ server.js
@@ -26,9 +26,16 @@ const app = express();
 const server = http.createServer(app);
 sockets.init(server);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 app.use(cookieParser());
 
 require('./auth/auth');
