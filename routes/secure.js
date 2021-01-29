f3e51df8f8dacf90cff1726b96f33219e8114a27
Terr1bus <qwant94@mail.ru>
2021-01-29 20:50:49 +0300

    fix(submit-game): remove third argument in update method

--- routes/secure.js
+++ routes/secure.js
@@ -4,15 +4,25 @@ const UserModel = require('../models/userModel');
 
 const router = express.Router();
 
-router.post('/submit-game', asyncMiddleware(async (req, res, next) => {
-  const { email, game } = req.body;
-  await UserModel.updateOne({ email }, { $push: { games: game } }, done);
-  res.status(200).json({ 'status': 'ok' });
-}));
+router.post(
+  '/submit-game',
+  asyncMiddleware(async (req, res, next) => {
+    const { email, game } = req.body;
+    const queryResult = await UserModel.updateOne(
+      { email },
+      { $push: { games: game } }
+      // done
+    );
+    res.status(200).json({ status: 'ok' });
+  })
+);
 
-router.get('/scores', asyncMiddleware(async (req, res, next) => {
-  const users = await UserModel.find({}, 'name games -_id');
-  res.status(200).json(users);
-}));
+router.get(
+  '/scores',
+  asyncMiddleware(async (req, res, next) => {
+    const users = await UserModel.find({}, 'name games -_id');
+    res.status(200).json(users);
+  })
+);
 
 module.exports = router;
